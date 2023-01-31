d70e857c685995616a3ae13f739f17c2c2d11bfa
Dan Kiuna <dkkiuna11@gmail.com>
2023-01-31 13:08:12 -0600

    remove ssr:true

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,10 @@ import '../styles/globals.css'
 import { Amplify } from "aws-amplify";
 import config from '../src/aws-exports'
 
-Amplify.configure({...config, ssr: true});
+Amplify.configure({
+  ...config,
+  //ssr: true
+});
 
 
 function MyApp({ Component, pageProps }) {
