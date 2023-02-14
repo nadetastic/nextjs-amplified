1eaa7b65d6e26ad20ff1233c6e4b7629cdc1fb63
Dan Kiuna <dkkiuna11@gmail.com>
2023-02-14 11:10:58 -0600

    empty GSSP method

--- pages/index.js
+++ pages/index.js
@@ -187,6 +187,10 @@ const HomeUI = () => {
  
 // export default HomeUI;
 
-
+export async function getServerSideProps(context) {
+  return {
+    props: {}, // will be passed to the page component as props
+  }
+}
 
 export default Home;
